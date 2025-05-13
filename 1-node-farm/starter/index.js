6671e803ab83f54f6b8af7bf2603fcccea58cac7
KostPh23 <76623123+KostPh23@users.noreply.github.com>
2025-05-13 15:33:29 +0300

    Update 1-node-farm/starter/index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -10,8 +10,16 @@ const fs = require("fs");
 
 // Non-blocking, asynchronous way
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR! 💥");
+
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
     console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, (err) => {
+        console.log("Your file has been written 😁");
+      });
+    });
   });
 });
 console.log("Will read file!");
