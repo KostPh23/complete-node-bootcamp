195ca2ba0a201301c7440ce8459cb08ab8f5f24b
KostPh23 <76623123+KostPh23@users.noreply.github.com>
2025-05-14 14:32:06 +0300

    Update 1-node-farm/starter/index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //////////////////////////////
 // FILES
@@ -31,10 +32,22 @@ const http = require("http");
 //////////////////////////////
 // SERVER
 
-// const server = http.createServer((req, res) => {
-//   res.end("Hello from the server!");
-// });
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
-// server.listen("8000", "127.0.0.1", () => {
-//   console.log("Listening to requests on port 8000");
-// });
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/products") {
+    res.end("This is the PRODUCTS");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen("8000", "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
