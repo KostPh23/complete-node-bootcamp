0c98c2cd6a43e695d6068247fcea712bc21e41eb
KostPh23 <76623123+KostPh23@users.noreply.github.com>
2025-05-28 01:46:13 +0300

    Update 3-asynchronous-JS/starter/index.js

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,16 +19,34 @@ const writeFilePro = (file, data) => {
   });
 };
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`, 'utf-8');
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file!');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
+/*
 readFilePro(`${__dirname}/dog.txt`, 'utf-8')
   .then(data => {
     console.log(`Breed: ${data}`);
-
     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
   })
   .then(res => {
     console.log(res.body.message);
-
-    return writeFilePro('dog-img.txt', res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);writeFilePro('dog-img.txt', res.body.message);
   })
   .then(() => {
     console.log('Random dog image saved to file!');
@@ -36,3 +54,4 @@ readFilePro(`${__dirname}/dog.txt`, 'utf-8')
   .catch(err => {
     console.log(err);
   });
+*/
